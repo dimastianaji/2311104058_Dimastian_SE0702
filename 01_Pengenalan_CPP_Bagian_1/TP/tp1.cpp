4f4e5d6400b3b9f5bd4b8d1cd115c8436956c460
Dimastian Aji Wibowo <147970502+dimastianaji@users.noreply.github.com>
2024-09-28 21:13:55 +0700

    Add files via upload

---
+++ 01_Pengenalan_CPP_Bagian_1/TP/tp1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string nama, nim;
+    cout << "Siapa nama anda? " ;
+    cin >> nama;
+    cout << "Berapa NIM anda? " ;
+    cin >> nim;
+    cout << "Nama saya: " << nama << endl;
+    cout << "NIM saya: " << nim << endl;
+    return 0;
+}
